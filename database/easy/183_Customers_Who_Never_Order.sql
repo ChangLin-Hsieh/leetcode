f45981a2f120b0c3f2c380b3c3af72abee65acc4
Allen Hsieh <allen355200@gmail.com>
2020-07-25 13:51:32 +0800

    183_Customers_Who_Never_Order.sql

---
+++ database/easy/183_Customers_Who_Never_Order.sql
@@ -0,0 +1,38 @@
+/*Problem
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+Solution: Left Join Orders to Customers, and find the empty order id*/
+
+select co.Name as Customers from
+(select 
+    c.Name as Name, 
+    o.id as Id from Customers c 
+    left join Orders o on c.Id = o.CustomerId) as co
+where co.Id is null
